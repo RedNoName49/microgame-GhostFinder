2ec46c850e72e93c287ec35c16bce8d35f8a6f88
RedNoName49 <Evanw649@gmail.com>
2026-09-20 03:11:48 -0500

    win and lose timer
    added win and lose timers (still have to make the lose timer scale with difficulty)

--- GhostGame/room_1.gd
+++ GhostGame/room_1.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
 var ghostChoice: int = randi_range(0, 3)
+
+var timer: float = 15
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	timer -= delta
+	if timer > 0:
+		print("time left:", snapped(timer, 0.01))
+	if timer <= 0:
+		print("you lose")
